e8cd354b10b8fbe94a4b5a88e2e3da91d83adfad
Mir Md. Asif Hossain <asif.hossain@therapservices.net>
2016-02-10 15:18:16 +0600

    Implement 0 1 knapsack algorithm

---
+++ src/com/mirasif/codeforces/algorithms/Knapsack.java
@@ -0,0 +1,37 @@
+package com.mirasif.codeforces.algorithms;
+
+/**
+ * @author asif.hossain
+ * @since 2/10/16
+ */
+public class Knapsack {
+
+    public static int zeroOneKnapsack(int capacity, int[] values, int[] weights) {
+        if (capacity == 0 || values.length == 0) {
+            return 0;
+        }
+        int[][] m = new int[values.length + 1][capacity + 1];
+        
+        for (int j = 0; j <= capacity; j++) {
+            m[0][j] = 0;
+        }
+        
+        for (int i = 1; i <= values.length; i++) {
+            for (int j = 0; j <= capacity; j++) {
+                if (weights[i-1] > j) {
+                    m[i][j] = m[i-1][j];
+                }
+                else {
+                    m[i][j] = Math.max(m[i-1][j], m[i-1][j-weights[i-1]] + values[i-1]);
+                }
+            }
+        }
+
+        return m[values.length][capacity];
+    }
+    
+    public static void main(String[] args) {
+        System.out.println(zeroOneKnapsack(5, new int[]{3, 4, 5, 6}, new int[]{2, 3, 4, 5}));
+
+    }
+}
---
+++ src/com/mirasif/codeforces/utils/ArrayUtils.java
@@ -0,0 +1,28 @@
+package com.mirasif.codeforces.utils;
+
+/**
+ * @author asif.hossain
+ * @since 2/10/16
+ */
+
+@SuppressWarnings("UnusedDeclaration")
+public class ArrayUtils {
+    public static void printArray(int[][] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                System.out.print(array[i][j] + "\t");
+            }
+            System.out.println();
+        }
+    }
+    public static void printArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + "\t");
+        }
+        System.out.println();
+    }
+    
+    public static void main() {
+        
+    }
+}
