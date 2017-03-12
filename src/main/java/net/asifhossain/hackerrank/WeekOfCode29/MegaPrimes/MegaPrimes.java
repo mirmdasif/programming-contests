717ef311063aa586d2df8e604e758df1662f8fe7
Mir Md. Asif Hossain <mirmdasif@gmail.com>
2017-03-12 17:27:17 +0600

    AC Megaprimes

--- src/main/java/net/asifhossain/hackerrank/WeekOfCode29/MegaPrimes/MegaPrimes.java
+++ src/main/java/net/asifhossain/hackerrank/WeekOfCode29/MegaPrimes/MegaPrimes.java
@@ -10,16 +10,6 @@ import java.util.Scanner;
  * @since 2/26/17.
  */
 public class MegaPrimes {
-    static boolean isPrime(long n) {
-        if(n < 2) return false;
-        if(n == 2 || n == 3) return true;
-        if(n%2 == 0 || n%3 == 0) return false;
-        long sqrtN = (long)Math.sqrt(n)+1;
-        for(long i = 6L; i <= sqrtN; i += 6) {
-            if(n%(i-1) == 0 || n%(i+1) == 0) return false;
-        }
-        return true;
-    }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -33,8 +23,11 @@ public class MegaPrimes {
         for (Long i = getNext(first); i <= last; i = getNextProbablePrime(i)) {
 
             if (i < first) continue;
+//            System.out.println(i);
 
-            if (Prime.isPrime(i)) {
+            BigInteger b = new BigInteger(i.toString());
+
+            if (b.isProbablePrime(10)) {
                 count++;
             }
         }
@@ -64,7 +57,6 @@ public class MegaPrimes {
                     a[i] = '2';
                 }
             } else if (con.get(a[i]) < a[i]) {
-
                 a[i] = con.get(a[i]);
                 while (++i < a.length) {
                     a[i] = '7';
@@ -103,83 +95,5 @@ public class MegaPrimes {
         }
 
         return Long.parseLong(new String(a));
-    }
-}
-
-class Prime {
-
-    // this is the RabinMiller test, deterministically correct for n < 341,550,071,728,321
-    // http://rosettacode.org/wiki/Miller-Rabin_primality_test#Python:_Proved_correct_up_to_large_N
-    public static boolean isPrime(BigInteger n, int precision) {
-
-        if (n.compareTo(new BigInteger("341550071728321")) >= 0) {
-            return n.isProbablePrime(precision);
-        }
-
-        int intN = n.intValue();
-        if (intN == 1 || intN == 4 || intN == 6 || intN == 8) return false;
-        if (intN == 2 || intN == 3 || intN == 5 || intN == 7) return true;
-
-        int[] primesToTest = getPrimesToTest(n);
-        if (n.equals(new BigInteger("3215031751"))) {
-            return false;
-        }
-        BigInteger d = n.subtract(BigInteger.ONE);
-        BigInteger s = BigInteger.ZERO;
-        while (d.mod(BigInteger.valueOf(2)).equals(BigInteger.ZERO)) {
-            d = d.shiftRight(1);
-            s = s.add(BigInteger.ONE);
-        }
-        for (int a : primesToTest) {
-            if (try_composite(a, d, n, s)) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    public static boolean isPrime(BigInteger n) {
-        return isPrime(n, 100);
-    }
-
-    public static boolean isPrime(int n) {
-        return isPrime(BigInteger.valueOf(n), 100);
-    }
-
-    public static boolean isPrime(long n) {
-        return isPrime(BigInteger.valueOf(n), 100);
-    }
-
-    private static int[] getPrimesToTest(BigInteger n) {
-        if (n.compareTo(new BigInteger("3474749660383")) >= 0) {
-            return new int[]{2, 3, 5, 7, 11, 13, 17};
-        }
-        if (n.compareTo(new BigInteger("2152302898747")) >= 0) {
-            return new int[]{2, 3, 5, 7, 11, 13};
-        }
-        if (n.compareTo(new BigInteger("118670087467")) >= 0) {
-            return new int[]{2, 3, 5, 7, 11};
-        }
-        if (n.compareTo(new BigInteger("25326001")) >= 0) {
-            return new int[]{2, 3, 5, 7};
-        }
-        if (n.compareTo(new BigInteger("1373653")) >= 0) {
-            return new int[]{2, 3, 5};
-        }
-        return new int[]{2, 3};
-    }
-
-    private static boolean try_composite(int a, BigInteger d, BigInteger n, BigInteger s) {
-        BigInteger aB = BigInteger.valueOf(a);
-        if (aB.modPow(d, n).equals(BigInteger.ONE)) {
-            return false;
-        }
-        for (int i = 0; BigInteger.valueOf(i).compareTo(s) < 0; i++) {
-            // if pow(a, 2**i * d, n) == n-1
-            if (aB.modPow(BigInteger.valueOf(2).pow(i).multiply(d), n).equals(n.subtract(BigInteger.ONE))) {
-                return false;
-            }
-        }
-        return true;
     }
 }
