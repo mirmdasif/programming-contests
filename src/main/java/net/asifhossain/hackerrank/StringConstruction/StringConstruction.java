a1458e2e587969fb50f735a1a6d201ad581a35cd
Mir Md. Asif Hossain <mirmdasif@gmail.com>
2017-09-24 16:56:40 +0600

    Hackerrank String Construction

---
+++ src/main/java/net/asifhossain/hackerrank/StringConstruction/StringConstruction.java
@@ -0,0 +1,23 @@
+package net.asifhossain.hackerrank.StringConstruction;
+
+import java.util.Scanner;
+
+public class StringConstruction {
+
+    static long stringConstruction(String s) {
+        // Complete this function
+        return s.chars().distinct().count();
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int q = in.nextInt();
+
+        for(int a0 = 0; a0 < q; a0++){
+            String s = in.next();
+            long result = stringConstruction(s);
+            System.out.println(result);
+        }
+        in.close();
+    }
+}
