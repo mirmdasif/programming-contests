49f879d03fb54995ff3723bcb80bb983efa221b7
Mir Md. Asif Hossain <mirmdasif@gmail.com>
2017-11-02 11:37:17 +0600

    Javarivisited fibonacci series

--- src/main/java/net/asifhossain/javarivisited/FibonacciSeries.java
+++ src/main/java/net/asifhossain/javarivisited/FibonacciSeries.java
@@ -1,7 +1,28 @@
 package net.asifhossain.javarivisited;
 
 /**
- * Created by asif.hossain on 10/16/17.
+ * @author asif.hossain
+ * @since 10/16/17.
  */
 public class FibonacciSeries {
+
+    public static void main(String[] args) {
+        fibonacci(10);
+    }
+
+    static void fibonacci(int n) {
+        int n1 = 1;
+        int n2 = 1;
+        if(n < 1) return;
+        System.out.println(n1);
+        System.out.println(n2);
+        while (n2 <= n) {
+            System.out.println(n1 + n2);
+            int next = n1 + n2;
+            n1 = n2;
+            n2 = next;
+        }
+
+
+    }
 }
