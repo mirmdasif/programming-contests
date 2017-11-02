ebded4724d0703b9fc9fea82f0126e347403df1d
Mir Md. Asif Hossain <mirmdasif@gmail.com>
2017-11-02 12:55:17 +0600

    Hackerrank SumVsXor

--- src/main/java/net/asifhossain/hackerrank/SumVsXor/Solution.java
+++ src/main/java/net/asifhossain/hackerrank/SumVsXor/Solution.java
@@ -1,7 +1,36 @@
 package net.asifhossain.hackerrank.SumVsXor;
 
+import java.util.Scanner;
+
 /**
- * Created by asif.hossain on 10/15/17.
+ * @author asif.hossain
+ * @since 10/15/17.
  */
 public class Solution {
+
+    static long solve(long n) {
+        // Complete this function
+        int nOfZeroBits = nZeroBit(n);
+
+        return 1L << nOfZeroBits;
+    }
+
+    private static int nZeroBit(long n) {
+        int count = 0;
+        while (n > 0) {
+            if((n & 1) == 0) {
+                count++;
+            }
+            n >>= 1;
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        long n = in.nextLong();
+        long result = solve(n);
+        System.out.println(result);
+    }
 }
