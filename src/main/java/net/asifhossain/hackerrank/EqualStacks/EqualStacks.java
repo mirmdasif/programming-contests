8908992dc285327915fa4a557d5a8b995b7f63e3
Mir Md. Asif Hossain <mirmdasif@gmail.com>
2017-11-05 11:42:00 +0600

    Improved permutaion

---
+++ src/main/java/net/asifhossain/hackerrank/EqualStacks/EqualStacks.java
@@ -0,0 +1,7 @@
+package net.asifhossain.hackerrank.EqualStacks;
+
+/**
+ * Created by asif.hossain on 11/2/17.
+ */
+public class EqualStacks {
+}
--- src/main/java/net/asifhossain/interviewcake/Permutation/Solution.java
+++ src/main/java/net/asifhossain/interviewcake/Permutation/Solution.java
@@ -1,10 +1,8 @@
 package net.asifhossain.interviewcake.Permutation;
 
 import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.LinkedHashSet;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 /**
  * @author asif.hossain
@@ -12,35 +10,34 @@ import java.util.stream.Collectors;
  */
 public class Solution {
     public static void main(String[] args) {
-        Set<String> perms = permutation("123");
+        Set<String> perms = permutation("ABC");
         System.out.println(perms);
     }
 
     private static Set<String> permutation(String str) {
-        List<Character> characters = str.chars().mapToObj(value -> (char) value).collect(Collectors.toList());
-        return permutation(characters);
-    }
+        Set<String> permutations = new LinkedHashSet<>();
 
-    private static Set<String> permutation(List<Character> characters) {
-        if (characters.isEmpty()) {
-            Set<String> emptyStringSet = new HashSet<>();
-            emptyStringSet.add("");
+        if(str == null) return permutations;
 
-            return emptyStringSet;
+        if("".equals(str)) {
+            permutations.add("");
+            return permutations;
         }
 
-        Set<String> strings = new HashSet<>();
-        for (int i = 0; i < characters.size(); i++) {
-            List<Character> tempChars = new LinkedList<>(characters);
-            char firstChar = tempChars.get(i);
-            tempChars.remove(i);
-            Set<String> stringSet = permutation(tempChars);
-
-            for (String s : stringSet) {
-                strings.add(firstChar + s);
+        char first = str.charAt(0);
+        for (String s : permutation(str.substring(1))) {
+            for (int i = 0; i <= s.length(); i++) {
+                permutations.add(insertChar(s, first, i));
             }
         }
 
-        return strings;
+        return permutations;
+    }
+
+    private static String insertChar(String s, char ch, int i) {
+        String begin = s.substring(0, i);
+        String end = s.substring(i);
+
+        return begin + ch + end;
     }
 }
