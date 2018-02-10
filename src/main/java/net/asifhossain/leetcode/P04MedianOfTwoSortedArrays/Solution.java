81c6312f3331534c40e2528a80a817a19a851f15
Mir Md. Asif Hossain <mirmdasif@gmail.com>
2018-02-10 13:25:48 +0600

    Refactored median of two sorted array

--- src/main/java/net/asifhossain/leetcode/P04MedianOfTwoSortedArrays/Solution.java
+++ src/main/java/net/asifhossain/leetcode/P04MedianOfTwoSortedArrays/Solution.java
@@ -7,15 +7,16 @@ import net.asifhossain.codeforces.utils.ArrayUtils;
  * @since 3/2/17.
  */
 public class Solution {
-    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+
+    double findMedianSortedArrays_On(int[] nums1, int[] nums2) {
         int[] num = new int[nums1.length + nums2.length];
 
         for (int i = 0, j = 0, k = 0; (i < nums1.length || j < nums2.length) && k <= (num.length - 1) / 2 + 1; k++) {
-            if(i >= nums1.length) {
+            if (i >= nums1.length) {
                 num[k] = nums2[j++];
-            } else if(j >= nums2.length) num[k++] = nums1[i++];
+            } else if (j >= nums2.length) num[k++] = nums1[i++];
             else {
-                if(nums1[i] <= nums2[j]) {
+                if (nums1[i] <= nums2[j]) {
                     num[k] = nums1[i++];
                 } else {
                     num[k] = nums2[j++];
@@ -25,7 +26,7 @@ public class Solution {
 
         if (num.length == 0) return 0;
         if (num.length % 2 == 0) {
-            return ((double) num[(num.length - 1) / 2] + (double) num[(num.length - 1) / 2 + 1])/ 2;
+            return ((double) num[(num.length - 1) / 2] + (double) num[(num.length - 1) / 2 + 1]) / 2;
         } else {
             return num[(num.length - 1) / 2];
         }
@@ -35,13 +36,13 @@ public class Solution {
 
         int[] num = new int[nums1.length + nums2.length];
 
-        for (int i = 0, j = 0, k = 0; i < nums1.length || j < nums2.length;) {
-            if(i >= nums1.length) {
+        for (int i = 0, j = 0, k = 0; i < nums1.length || j < nums2.length; ) {
+            if (i >= nums1.length) {
                 num[k++] = nums2[j++];
-            } else if(j >= nums2.length) {
+            } else if (j >= nums2.length) {
                 num[k++] = nums1[i++];
             } else {
-                if(nums1[i] <= nums2[j]) {
+                if (nums1[i] <= nums2[j]) {
                     num[k++] = nums1[i++];
                 } else {
                     num[k++] = nums2[j++];
@@ -55,12 +56,12 @@ public class Solution {
     public static void main(String[] args) {
         Solution s = new Solution();
 
-        System.out.println(s.findMedianSortedArrays(new int[]{1, 2}, new int[]{3, 4}));
-        System.out.println(s.findMedianSortedArrays(new int[]{1, 3, 7}, new int[]{4, 6}));
-        System.out.println(s.findMedianSortedArrays(new int[]{1, 3}, new int[]{2}));
-        System.out.println(s.findMedianSortedArrays(new int[]{}, new int[]{}));
-        System.out.println(s.findMedianSortedArrays(new int[]{1}, new int[]{}));
-        System.out.println(s.findMedianSortedArrays(new int[]{}, new int[]{1}));
+        System.out.println(s.findMedianSortedArrays_On(new int[]{1, 2}, new int[]{3, 4}));
+        System.out.println(s.findMedianSortedArrays_On(new int[]{1, 3, 7}, new int[]{4, 6}));
+        System.out.println(s.findMedianSortedArrays_On(new int[]{1, 3}, new int[]{2}));
+        System.out.println(s.findMedianSortedArrays_On(new int[]{}, new int[]{}));
+        System.out.println(s.findMedianSortedArrays_On(new int[]{1}, new int[]{}));
+        System.out.println(s.findMedianSortedArrays_On(new int[]{}, new int[]{1}));
 
         ArrayUtils.printArray(s.merge(new int[]{1, 3}, new int[]{2}));
     }
