8707d28bd5924bad7fc39f63d4ac23f88ce27be6
Mir Md. Asif Hossain <mirmdasif@gmail.com>
2018-03-29 20:11:43 +0600

    Leet code problem 2 AC

--- src/main/java/net/asifhossain/leetcode/P002/Solution.java
+++ src/main/java/net/asifhossain/leetcode/P002/Solution.java
@@ -9,36 +9,35 @@ class ListNode {
     }
 }
 
-public class Solution {
-
+class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        return addNumber(l1, l2, 0);
-    }
-
-    private ListNode addNumber(ListNode l1, ListNode l2, int remainder) {
-        if(l1 == null && l2 == null) {
-            if(remainder == 0) return null;
-
-            return new ListNode(remainder);
-        }
-
-        int n1 = 0;
-
-        if(l1 != null) {
-            n1 = l1.val;
-            l1 = l1.next;
-        }
-
-        int n2 = 0;
-
-        if(l2 != null) {
-            n2 = l2.val;
-            l2 = l2.next;
-        }
-
-        ListNode node = new ListNode((n1 + n2 + remainder) % 10);
-        node.next = addNumber(l1, l2, (n1 + n2 + remainder) / 10);
-
-        return node;
+        ListNode sum = null;
+        ListNode last = null;
+        int mod = 0;
+        while(l1 != null || l2 != null || mod != 0) {
+            int s1 = 0;
+            int s2 = 0;
+            if (l1 != null) {
+                s1 = l1.val;
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                s2 = l2.val;
+                l2 = l2.next;
+            }
+            
+            ListNode node = new ListNode((s1 + s2 + mod) % 10);
+            if (sum == null) {
+                sum = node;
+                last = node;
+            } else {
+                last.next = node;
+                last = node;
+            }
+            
+            mod = (s1 + s2 + mod) / 10;
+        } 
+        
+        return sum;
     }
 }
