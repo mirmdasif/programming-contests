23e294657d47e8b91f6cccc522a0b1bfe034a046
Mir Md. Asif Hossain <mir.md.asif@gmail.com>
2021-09-10 00:55:19 +0200

    Create LonelyInteger.kt

---
+++ src/main/java/net/asifhossain/hackerrank/1WeekPrep/LonelyInteger.kt
@@ -0,0 +1,51 @@
+import java.io.*
+import java.math.*
+import java.security.*
+import java.text.*
+import java.util.*
+import java.util.concurrent.*
+import java.util.function.*
+import java.util.regex.*
+import java.util.stream.*
+import kotlin.collections.*
+import kotlin.comparisons.*
+import kotlin.io.*
+import kotlin.jvm.*
+import kotlin.jvm.functions.*
+import kotlin.jvm.internal.*
+import kotlin.ranges.*
+import kotlin.sequences.*
+import kotlin.text.*
+
+/*
+ * Complete the 'lonelyinteger' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY a as parameter.
+ */
+
+fun lonelyinteger(a: Array<Int>): Int {
+    val array = IntArray(101)
+    
+    for (number in a) {
+    
+        array[number] += 1
+    }
+    
+    for (number in 0..array.size - 1) {
+    
+        if(array[number] == 1) return number
+    }
+    
+    return 0
+}
+
+fun main(args: Array<String>) {
+    val n = readLine()!!.trim().toInt()
+
+    val a = readLine()!!.trimEnd().split(" ").map{ it.toInt() }.toTypedArray()
+
+    val result = lonelyinteger(a)
+
+    println(result)
+}
