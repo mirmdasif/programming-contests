346ae24dfb9fb54dda0c396ab44b1ce67b2bf3df
Mir Md. Asif Hossain <mir.md.asif@gmail.com>
2021-09-11 21:46:59 +0200

    Create CountSort1.kt

---
+++ src/main/java/net/asifhossain/hackerrank/1WeekPrep/CountSort1.kt
@@ -0,0 +1,48 @@
+import java.io.*
+import java.math.*
+import java.security.*
+import java.text.*
+import java.util.*
+import java.util.concurrent.*
+import java.util.function.*
+import java.util.regex.*
+import java.util.stream.*
+import kotlin.collections.*
+import kotlin.comparisons.*
+import kotlin.io.*
+import kotlin.jvm.*
+import kotlin.jvm.functions.*
+import kotlin.jvm.internal.*
+import kotlin.ranges.*
+import kotlin.sequences.*
+import kotlin.text.*
+
+/*
+ * Complete the 'countingSort' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+fun countingSort(arr: Array<Int>): Array<Int> {
+    // Write your code here
+    
+    val frequencyArray = Array<Int>(100) { 0 }
+    
+    for(number in arr) {
+        frequencyArray[number]++
+    }
+    
+    return frequencyArray
+
+}
+
+fun main(args: Array<String>) {
+    val n = readLine()!!.trim().toInt()
+
+    val arr = readLine()!!.trimEnd().split(" ").map{ it.toInt() }.toTypedArray()
+
+    val result = countingSort(arr)
+
+    println(result.joinToString(" "))
+}
